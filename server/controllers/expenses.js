ffd1ef590f44a4733f08a7e3e1a7dbbe79e6df3e
Sana Matusala <matusalasana@gmail.com>
2026-03-23 23:45:41 +0300

    client folder separately created

--- server/controllers/expenses.js
+++ server/controllers/expenses.js
@@ -7,7 +7,7 @@ export const getExpenses = async (req, res) => {
       SELECT t.*, c.name as category_name, c.color_code 
       FROM transactions t
       LEFT JOIN categories c ON t.category_id = c.id
-      WHERE t.type = 'expense'
+      WHERE t.user-id = 'user-id' AND t.type = 'expense'
       ORDER BY t.transaction_date DESC
     `;
     res.json(expenses);
@@ -16,3 +16,21 @@ export const getExpenses = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
+
+
+
+
+
+
+const expenses = await sql`
+  SELECT 
+    t.*,                   -- 1. Grab every column from the Transaction (amount, date, etc.)
+    c.name as category_name, -- 2. Grab the 'name' from Categories, but call it 'category_name'
+    c.color_code           -- 3. Grab the 'color_code' from Categories
+  FROM transactions t
+  LEFT JOIN categories c ON t.category_id = c.id -- 4. Connect the two tables
+  WHERE t.user_id = ${userId} -- 5. Only show data belonging to the logged-in user
+  AND t.type = 'expense'      -- 6. Filter out 'income' or 'transfers'
+  ORDER BY t.transaction_date DESC -- 7. Show newest first
+`;
+
