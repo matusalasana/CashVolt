9f4656c457ff96d59ee717fb52f0b37135ef8c36
Sana Matusala <matusalasana@gmail.com>
2026-03-29 15:55:56 +0300

    log out functionality implemented

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+
 
 const Settings = () => {
+  
+  const navigate = useNavigate();
+  const handleLogout = () => {
+  localStorage.removeItem("token");
+  navigate("/login");
+};
+
   return (
-    <div>Settings</div>
+    <div>
+      <p className="text-2xl">Settings page</p>
+    
+    <button onClick={handleLogout} className="border-1 text-red-700 border-red-200">
+      Logout
+    </button>
+    </div>
   )
 }
 
