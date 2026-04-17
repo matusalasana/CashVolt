9abeac319e36d47af9126fb09f332b1f84bda749
Sana Matusala <matusalasana@gmail.com>
2026-04-17 15:13:12 +0300

    frontend domain changed

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://cash-volt-kr14.vercel.app";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://cash-volt.vercel.app";
 export const FRONTEND_LOCALHOST_URL = process.env.FRONTEND_LOCALHOST_URL || "http://localhost:5173";
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
