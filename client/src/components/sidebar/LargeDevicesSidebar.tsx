c1b87ce42a87b260b08ff4fa5a2a6bd83eade4af
Sana Matusala <matusalasana@gmail.com>
2026-04-30 11:54:18 +0300

    large screen sidebar name fixed 2

--- client/src/components/sidebar/LargeDevicesSidebar.tsx
+++ client/src/components/sidebar/LargeDevicesSidebar.tsx
@@ -67,7 +67,7 @@ const LargeDevicesSidebar = () => {
       </nav>
 
       {/* User Section */}
-      <div className="mt-4 flex mb-20 gap-3 justify-center items-center pt-4 border-t border-base-200 pb-12">
+      <div className="mt-4 flex gap-3 pt-4 border-t border-base-200 pb-12">
         {user?.profile_picture ? (
           <img
             src={user.profile_picture}
